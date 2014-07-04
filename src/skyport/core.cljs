eefb369216823114bb190c6031c033d682ae7f8e
Bjarte Johansen <bjarte.johansen@gmail.com>
2014-07-04 13:07:33 +0200

    add player to map

--- src/skyport/core.cljs
+++ src/skyport/core.cljs
@@ -20,7 +20,7 @@
                                                 :level 1}
                              :health 100
                              :score 0
-                             :position "0,2"}]}))
+                             :position [0 2]}]}))
 
 ;; (go
 ;;   (let [{:keys [ws-channel error]}
@@ -42,30 +42,59 @@
                  (str x_i " " y_i))]
     (dom/polygon #js {:points (str/join ", " points)})))
 
+(defn display [show]
+  (if show
+    #js {}
+    #js {:display "none"}))
+
+(defn hexagon-view [{:keys [x y size label]} owner]
+  (reify
+    om/IRender
+    (render [_]
+      (dom/g #js {:style (display (not= "V" label))}
+             (hexagon {:x x :y y :size size})
+             (dom/text #js {:x x :y y}
+                       label)))))
+
+(defn coords-to-pixel [[j k] size offset]
+  [(+ offset (* size
+                (/ 3 2)
+                (+ 2 (- k j))))
+   (+ offset (* (/ size 2)
+                (js/Math.sqrt 3)
+                (+ k (- j 2))))])
+
+(defn player-view [player]
+  (reify
+    om/IRender
+    (render [_]
+      (let [[x y] (coords-to-pixel (:position player) 25 75)]
+        (dom/circle #js {:cx x
+                         :cy y
+                         :r 10
+                         :fill "blue"})))))
+
 (defn world-view [state owner]
   (reify
     om/IRender
     (render [_]
-      (dom/div {}
-       (dom/svg #js {:width "100%" :height "100%"}
+      (dom/div #js {:id "world"}
+       (dom/svg #js {:width "50%" :height "50%"}
         (apply dom/g nil
-               (flatten
-                (for [j (range 3)
-                      k (range 3)
-                      :let [size 25
-                            offset 130
-                            x (+ offset (* size
-                                           (/ 3 2)
-                                           (+ 3 (- k j 1))))
-                            y (+ offset (* (/ size 2)
-                                           (js/Math.sqrt 3)
-                                           (+ j (- k 2))))]]
-                  (list (hexagon {:x x
+               (om/build-all hexagon-view
+                             (let [data (:data state)]
+                               (for [j (range (count data))
+                                     k (range (count (data j)))
+                                     :let [size 25
+                                           offset 75
+                                           [x y] (coords-to-pixel [j k] 25 75)]]
+                                 {:x x
                                   :y y
-                                  :size size})
-                        (dom/text #js {:x x
-                                       :y y}
-                                  (get-in state [:data j k])))))))))))
+                                  :label (get-in data [j k])
+                                  :size size}))))
+        (apply dom/g nil
+               (om/build-all player-view
+                             (:players state))))))))
 
 (om/root world-view state
   {:target (.getElementById js/document "app")})
