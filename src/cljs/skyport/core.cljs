280d3b1d960d3e570774d6db82d72c0a55692dd7
Bjarte Johansen <bjarte.johansen@gmail.com>
2014-07-21 15:12:17 +0200

    cljs/skyport.core: implement mortar shot

--- src/cljs/skyport/core.cljs
+++ src/cljs/skyport/core.cljs
@@ -47,36 +47,56 @@
   (mapv (comp js/parseInt str/trim)
         (str/split coords ",")))
 
+(defn player! [name]
+  (->> @state
+       :players
+       (drop-while (comp (partial not= name)
+                         :name))
+       first))
+
+(defn direction->coord [dir]
+  (case dir
+    "up"         [-1 -1]
+    "left-up"    [ 0 -1]
+    "right-up"   [-1  0]
+    "down"       [ 1  1]
+    "left-down"  [ 1  0]
+    "right-down" [ 0  1]))
+
 (defmulti handle-action :type)
 
 (defmethod handle-action "laser"
   [{:keys [from stop] :as action}]
-  (let [start (->> @state
-                  :players
-                  (drop-while (comp (partial not= from)
-                                    :name))
-                  first
-                  :position)]
-    (swap! state assoc :action
-           (assoc action :start start :stop (parse-coords stop)))))
+  (swap! state assoc :action
+         (assoc action
+           :start (:position (player! from))
+           :stop (parse-coords stop))))
 
 (defmethod handle-action "droid"
-  [action]
-  (swap! state assoc :action action))
+  [{:keys [sequence] :as action}]
+  (swap! state assoc :action
+         (assoc action
+           :sequence (mapv direction->coord
+                           sequence))))
 
 (defmethod handle-action "mortar"
-  [action]
-  (swap! state assoc :action action))
+  [{:keys [from coordinates]}]
+  (let [player (player! from)
+        mortar (first
+                (drop-while (comp (partial not= "mortar") :name)
+                            (select-keys player
+                                         [:primary-weapon
+                                          :secondary-weapon])))]
+    (swap! state assoc :action
+           {:coord (mapv +
+                         (:position player)
+                         (parse-coords coordinates))
+            :radius (inc
+                     (:level mortar))})))
 
 (defmethod handle-action "move"
   [{:keys [direction from]}]
-  (let [[x y] (case direction
-                "up"         [-1 -1]
-                "left-up"    [ 0 -1]
-                "right-up"   [-1  0]
-                "down"       [ 1  1]
-                "left-down"  [ 1  0]
-                "right-down" [ 0  1])]
+  (let [[x y] (direction->coord direction)]
     (swap! state update-in [:players]
            (partial mapv (fn [{:keys [name position] :as player}]
                            (if-not (= name from)
@@ -108,8 +128,6 @@
 (defmethod handle-message! :default [message]
   ;; Do nothing.
   (println message))
-
-
 
 (defn timeout [ms]
   (let [c (chan)]
@@ -145,13 +163,16 @@
                 "E" "explosium"
                 "R" "rubidium"
                 "C" "scrap"
-                "O" "rock"})
+                "O" "rock"
+                "D" "dmg"})
 
 (def TILE-SIZE 25)
 (def X-OFFSET 475)
 (def Y-OFFSET 65)
 
-(defn hexagon [{:keys [size x y class]}]
+(defn hexagon
+  "Creates a polygon with six equal sides."
+  [{:keys [size x y class]}]
   (let [points (for [i (range 6)
                      :let [angle (* 2 (/ js/Math.PI 6) i)
                            x_i (+ x (* TILE-SIZE (js/Math.cos angle)))
@@ -188,7 +209,7 @@
                   (js/Math.sqrt 3)
                   (+ k (- j 2))))])
 
-(defn laser-shot [{:keys [start stop]}]
+(defn laser-shot [{:keys [start stop]} owner]
   (html
    (let [[x_1 y_1] (coords-to-pixel start)
          [x_2 y_2] (coords-to-pixel stop)
@@ -209,6 +230,40 @@
         :fill "url(#laser-shot)"}]])))
 
 
+(defn mortar-shot [{:keys [coord radius]} owner]
+  (html
+   (let [circle [[ 1  0]
+                 [ 1  1]
+                 [ 0  1]
+                 [-1  0]
+                 [-1 -1]
+                 [ 0 -1]]
+         ;; The way we find the points is that we take what we know is
+         ;; 1 complete circle around the given point and interleave it
+         ;; with itself for each increase in the radius. To make the
+         ;; path correctly we move the point up one hexagon each turn
+         ;; around the point, including the first round. We then take
+         ;; the current coordinate and add move it around the
+         ;; path. Lastly, because the each circle is completes back at
+         ;; its origin we remove all the extra points.
+         hits (->> circle
+                   repeat
+                   (reductions interleave)
+                   (take radius)
+                   (interleave (repeat '([-1 -1])))
+                   (apply concat)
+                   (reductions (partial mapv +) coord)
+                   distinct)]
+     [:g (om/build-all hexagon-view
+                       (for [[j k] hits
+                             :let [[x y] (coords-to-pixel [j k])]]
+                         {:x x
+                          :y y
+                          :size TILE-SIZE
+                          :label "D"
+                          :position [j k]})
+                       {:key :position})])))
+
 (defn player-view [{:keys [name position text]} owner]
   (reify
     om/IRender
@@ -223,16 +278,22 @@
                     :r (/ TILE-SIZE 2)}]])))))
 
 (defmulti action-view :type)
+(defmulti action-view
+  (fn [action _] (:type action)))
+
 (defmethod action-view "laser"
-  [shot]
-  (laser-shot shot))
+  [action owner]
+  (laser-shot action owner))
+
 (defmethod action-view "droid"
-  [action]
-  (html [:g]))
+  [action owner]
+  (droid-shot action owner))
+
 (defmethod action-view "mortar"
-  [action]
-  (html [:g]))
-(defmethod action-view :default [_]
+  [action owner]
+  (mortar-shot action owner))
+
+(defmethod action-view :default [_ _]
   (html [:g]))
 
 (defn world-view [state owner]
@@ -257,9 +318,9 @@
                            {:x x
                             :y y
                             :label (get-in data [j k])
-                            :key [j k]
+                            :position [j k]
                             :size TILE-SIZE}
-                           {:key :key}))]
+                           {:key :position}))]
            [:g (om/build-all player-view (:players state) {:key :name})]
            (om/build action-view (:action state))]])))))
 
